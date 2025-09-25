2581ed55acb9db83e6b5627cb2659123c8600ad9
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2025-09-24 20:02:32 -0400

    Update Aulas

---
+++ aula04/CadastroLocal/App.js
@@ -0,0 +1,23 @@
+import { NavigationContainer } from '@react-navigation/native';
+ import { createStackNavigator } from '@react-navigation/stack';
+ import React from 'react';
+ import ProdutoForm from './ProdutoForm';
+ import ProdutoLista from './ProdutoLista'
+
+ const Stack = createStackNavigator();
+
+ export default function App() {
+     return (
+     <NavigationContainer>
+         <Stack.Navigator initialRouteName='ListaProd'>
+         <Stack.Screen name='ListaProd'
+             options={{title: 'Listagem de Produtos'}}
+             component={ProdutoLista} />
+         <Stack.Screen name='NovoProd'
+             options={{title: 'Novo Produto'}}
+             component={ProdutoForm} />
+         </Stack.Navigator>
+     </NavigationContainer>
+     );
+ }
+                         
