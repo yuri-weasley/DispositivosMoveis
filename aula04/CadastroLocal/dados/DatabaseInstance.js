80966cc4466cb13ee2501f3caf040ba4a4b06ae0
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2025-09-26 17:20:49 -0400

    Update aula04

--- aula04/CadastroLocal/dados/DatabaseInstance.js
+++ aula04/CadastroLocal/dados/DatabaseInstance.js
@@ -1,5 +1,18 @@
-import { openDatabase } from 'react-native-sqlite-storage';
+import Realm from 'realm';
 
- var db = openDatabase({ name: 'LojaDatabase.db' });
+ var db = new Realm({
+     path: 'ProdutosDB.realm',
+     schema: [
+         {
+             name: 'Produto',
+             primaryKey: 'codigo',
+             properties: {
+                 codigo: 'int', 
+                 nome: 'string', 
+                 quantidade: 'int',
+         },
+         },
+     ],
+     });
 
  export default db;
--- aula04/CadastroLocal/dados/GestorDados.ts
+++ aula04/CadastroLocal/dados/GestorDados.ts
@@ -5,48 +5,24 @@ OBS.: Cada interrogação é um parâmetro que será substituído por um valor n
 */
 
 import db from './DatabaseInstance';
- import { Produto } from './Produto';
-
- const sqlCreate =
-         'CREATE TABLE IF NOT EXISTS PRODUTO('+
-         'CODIGO INTEGER PRIMARY KEY, '+
-         ' NOME VARCHAR(20), QUANTIDADE INTEGER)';
- const sqlInsert =
-         'INSERT INTO PRODUTO ( CODIGO, NOME, QUANTIDADE )'+
-         ' VALUES (?,?,?)';
- const sqlDelete =
-         'DELETE FROM PRODUTO WHERE CODIGO=?';
- const sqlSelect =
-         'SELECT * FROM PRODUTO';
+import { Produto } from './Produto';
 
  class GestorDados {
-     private async criarBanco(){
-     const database = await db;
-     database.transaction(txn => txn.executeSql(sqlCreate,[]));
-     }
      public async remover(chave: string){
-     (await db).transaction(txn =>
-             txn.executeSql(sqlDelete,[parseInt(chave)]));
+     db.write(()=>
+         db.delete(db.objects('Produto')
+                     .filtered('codigo = $0', parseInt(chave)))
+     );
      }
      public async adicionar(produto: Produto){
-     (await db).transaction(txn => txn.executeSql(sqlInsert,
-             [produto.codigo,produto.nome,produto.quantidade]));
+     db.write(() => db.create('Produto', produto));
      }
-     public async obterTodos(
-             useRetorno: (produtos: Array<Produto>) => void){
+     public async obterTodos(): Promise<Array<Produto>>{
      let objetos = [];
-     (await db).transaction((txn) => txn.executeSql(sqlSelect,[],
-         (txn, results) => {
-         for (let i = 0; i < results.rows.length; ++i) {
-             let linha = results.rows.item(i)
-             let produto: Produto = new Produto(
-                 linha.CODIGO, linha.NOME, linha.QUANTIDADE);
-             objetos.push(produto);
-         }
-         useRetorno(objetos);
-         if(objetos.length<1)
-             this.criarBanco();
-         }));
+     for(let obj of db.objects<Produto>('Produto')){
+         objetos.push(JSON.parse(JSON.stringify(obj)));
+     }
+     return objetos;
      }
  }
 
